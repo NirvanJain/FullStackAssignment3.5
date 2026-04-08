d16412deaddcdd0dac98056cef7fb2e1a9153543
Nirvan Jain <jainnirvan070@gmail.com>
2026-04-08 11:57:47 +0530

    First Commit

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+    return <h1>Dashboard (Protected in future)</h1>;
+  }
+  
+  export default Dashboard;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Welcome to Creator Platform 🚀</h1>;
+  }
+  
+  export default Home;
